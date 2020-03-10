92d6124bc4b439c330406648ae80220d354ed80f
sangwon21 <junothyme@gmail.com>
2020-03-11 00:31:46 +0900

    Feat: 동전 가치 숫자와 이름 매핑
    - 자판기에서 사용하는 동전 가치(숫자)와 그 가치의 이름(문자)를
    맵핑(mapping)했다.
    - 이는 상호 변환을 용이하게 하기 위함이다.

--- util.js
+++ util.js
@@ -10,3 +10,23 @@ export const BUTTON_ID = {
   FIVE_THOUSAND_WON: "five-thousand-won",
   TEN_THOUSAND_WON: "ten-thousand-won"
 };
+
+export const STR_TO_NUM = {
+  ten: 10,
+  fifty: 50,
+  hundred: 100,
+  fiveHundred: 500,
+  thousand: 1000,
+  fiveThousand: 5000,
+  tenThousand: 10000
+};
+
+export const NUM_TO_STR = {
+  10: "ten",
+  50: "fifty",
+  100: "hundred",
+  500: "fiveHundred",
+  1000: "thousand",
+  5000: "fiveThousand",
+  10000: "tenThousand"
+};
