0ea57cb5a9949fa6ea7aa05636947a5800aec6b0
sangwon21 <junothyme@gmail.com>
2020-03-10 19:18:45 +0900

    Feat: 옵저버 패턴을 적용하기 위한 Model Class 작성
    - 구독과 발행 등 객체 간 관계를 설정하기 위하여 Model Class를 작성했다.
    - Model Class는 특정 View와 연결된 Model에서 상속받아 사용한다.

---
+++ model.js
@@ -0,0 +1,16 @@
+class Model {
+  constructor() {
+    this.models = new Set();
+  }
+  subscribe(callback) {
+    this.models.add(callback);
+  }
+  unsubscribe(model) {
+    this.models = [...this.models].filter(subscriber => subscriber !== model);
+  }
+  notify([data]) {
+    this.models.forEach(callback => callback(data));
+  }
+}
+
+export default Model;
